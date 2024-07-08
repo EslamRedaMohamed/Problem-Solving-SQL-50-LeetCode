7a631f38eaed402703d250c6ff0df828a3d2830e
Eslam Reda-إسلام رضا <104031370+EslamRedaMohamed@users.noreply.github.com>
2024-07-08 12:08:37 +0300

    Time: 1065 ms (54.05%), Space: 0B (100.00%) - LeetHub

---
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select Employee.name , Bonus.bonus
+from Employee
+left outer join Bonus
+on Bonus.empId = Employee.empId
+where Bonus.bonus < 1000 or  Bonus.bonus is null;
